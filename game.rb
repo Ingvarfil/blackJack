5616db1a4f31ca6968d3206fae16ca7341fd5e25
ingvarfil <ingvarfil.dev@gmail.com>
2023-03-06 16:18:18 +0300

    Debugging

--- game.rb
+++ game.rb
@@ -22,6 +22,11 @@ class Game
 
   loop do
     game.start
+    game.distribution
+    game.bet
+    game.gamer_action
+    game.result
+    game.end_game
     
   end
 
@@ -31,6 +36,83 @@ class Game
     @dealer.hand.cards = []    
   end
 
+  def distribution
+    2.times do
+      @gamer.take_card(@deck.give_a_card)
+      @dealer.take_card(@deck.give_a_card)
+    end
+    @dealer.hand.points
+    @gamer.hand.points
+  end
+
+  def bet
+    @gamer.money -= 10
+    @dealer.money -= 10
+    @interface.show_desk(@gamer.show_name, @gamer.hand.show_cards, @gamer.hand.score, @gamer.show_money)
+    @interface.show_desk('Dealer', '***', '***', @dealer.show_money)
+  end
+
+  def result
+    draw if @gamer.hand.score > 21 && @dealer.hand.score > 21
+    draw if @gamer.hand.score == @dealer.hand.score
+    win if @gamer.hand.score > @dealer.hand.score && @gamer.hand.score <= 21
+    win if @dealer.hand.score > 21 && @gamer.hand.score <= 21
+    lose if @gamer.hand.score > 21 && @dealer.hand.score <= 21
+    lose if @dealer.hand.score > @gamer.hand.score && @dealer.hand.score <= 21
+    desk
+  end
+
+  def gamer_action
+    begin
+      @interface.action
+    rescue StandardError => e
+      puts e.message
+      retry
+    end
+    @gamer.take_card(@deck.give_a_card) if @interface.input == 1
+    @gamer.hand.points
+    dealer_action
+  end
+
+  def dealer_action
+    if @dealer.hand.score >= 17 || @interface.input == 2
+      nil
+    else
+      @dealer.take_card(@deck.give_a_card)
+      @dealer.hand.points
+    end
+  end
+
+  def desk
+    @interface.show_desk(@gamer.show_name, @gamer.hand.show_cards, @gamer.hand.score, @gamer.show_money)
+    @interface.show_desk(@dealer.name, @dealer.hand.show_cards, @dealer.hand.score, @dealer.show_money)
+  end
+
+  def end_game
+    @interface.no_money(@gamer.show_name) if @gamer.money <= 0
+    @interface.no_money(@dealer.name) if @dealer.money <= 0
+    @interface.exit_game
+  rescue StandardError => e
+    puts e.message
+    retry
+  end
+
+  def lose
+    @dealer.money += 20
+    @interface.lose
+  end
+
+  def win
+    @gamer.money += 20
+    @interface.win
+  end
+
+  def draw
+    @dealer.money += 10
+    @gamer.money += 10
+    @interface.draw
+  end
+  
   def validate_name
     raise 'Wrong name' if @gamer.name.nil? || @gamer.name.strip.empty?   
   end
--- interface.rb
+++ interface.rb
@@ -1,8 +1,47 @@
 class Interface
 
   def ask_name
-    puts 'BlackJack game!'
-    print 'Enter yor name: '  
+    puts '==== BlackJack game ===='
+    print 'Hi! Enter yor name: '  
+  end
+
+  def show_desk(name, cards, points, money)
+    puts
+    puts "#{name} cards: #{cards}"
+    puts "points:#{points} money: #{money}"
+    puts
+  end
+
+  def action
+    puts '1.Take card 2.Show cards 3.Skip turn'
+    @input = gets.to_i
+    variants = [1, 2, 3]
+    raise 'Wrong input!' unless variants.include?(input)
+  end
+
+  def lose
+    puts '==> You lose.'
+  end
+
+  def win
+    puts '==> You win.'
+  end
+
+  def draw
+    puts '==> It is draw.'
+  end
+
+  def no_money(name)
+    puts "#{name} have no money!"
+    exit
+  end
+
+  def exit_game
+    puts 'Want to go out? (Y/N)'
+    input = gets.chomp.upcase
+    answers = ['Y', 'N']
+    raise 'Wrong entry!' unless answers.include?(input)
+    exit if input == 'Y'
   end
  
 end
